a4d6e9525fc5fa941fbc079964bc4199e51aad08
ErnestoPelayo <ernesto.p@itmexicali.edu.mx>
2024-08-04 01:32:45 -0700

    style added for 2 styles view albums and artist

--- src/components/CardArtist.tsx
+++ src/components/CardArtist.tsx
@@ -1,21 +1,43 @@
+import {TriangleRightIcon} from '@primer/octicons-react'
+import { Link } from 'react-router-dom'
+import { ArtistClass } from '../Types'
+import { useEffect, useState } from 'react'
+import { Artist } from '../helpers/Artist'
 
 type propsCartArtist = {
-  name:string,
-  img:string
+  id:ArtistClass['id'],
+  name:ArtistClass['nombre_artista'],
+  img:ArtistClass['imagen'],
+  artist?:ArtistClass['id'],
+  style : number
 }
 
-const CardArtist = ({name,img}:propsCartArtist) => {
-  console.log(img)
+const CardArtist = ({id,name,img,style,artist}:propsCartArtist) => {
+
+  const [state,setState]= useState<ArtistClass[]>([])
+
+  useEffect(()=>{
+    artist ? setState(Artist.filter(item=>item.id === artist)): []
+  },[])
+
   return (
-    <div className=" rounded-lg relative text-white w-48 p-2 hover:bg-black-1003 group">
-      <img className="shadow-sm shadow-black-1001  rounded-full items-center justify-center justify-items-center" src={img} alt={name}/>
-        <button
-        className=" text-2xl justify-center items-center justify-items-center px-3 py-1.5 absolute top-2/3 right-4 transform -translate-y-2/3 rounded-full bg-green-500 text-black-1000  opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-        ►
-      </button>
-      <p className="mt-2 text-md">{name}</p>
-      <p className="mt-1 text-sm text-neutral-400">Artist</p>
-    </div>
+    <Link to={`/artist/${id}`}>
+        <div className=" rounded-lg relative text-white w-48 p-2 hover:bg-black-1003 group">
+          <img  className={`shadow-sm shadow-black-1001 ${style === 1 ? 'rounded-full' : 'rounded-lg'} items-center justify-center justify-items-center`}
+             src={img} 
+            alt={name}/>  
+            <button
+            className=" px-2 py-1.5 absolute top-2/3 right-4 transform -translate-y-2/3 rounded-full bg-green-500 text-black-1000  opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+                  <TriangleRightIcon size={40}  />
+          </button>
+          <p className="mt-2 text-md">{name}</p>
+          {
+            style ===1 ?
+              <p className="mt-1 text-sm text-neutral-400">Artist</p> :
+              <p className="mt-1 text-sm text-neutral-400">{state[0].nombre_artista}</p>
+          }
+        </div>
+    </Link>
   )
 }
 
