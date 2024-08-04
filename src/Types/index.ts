514aec24141a701545f59f51856e76a7ced3aeff
ErnestoPelayo <ernesto.p@itmexicali.edu.mx>
2024-08-04 01:40:31 -0700

    changed model for all classe becase the dabase changed structure

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,33 +1,34 @@
+
 export type ArtistClass = {
-    id: number,
+    id: string,
     nombre_artista: string,
     tipo_musica: string,
-    imagen: string
+    imagen: string,
+    preset :number
 }
 
 export type AlbumClass = {
-        id_album:number,
+        id_cantante :ArtistClass['id']
+        id_album:string,
         nombre_album: string,
         fecha_lanzamiento: string,
         activo :boolean
         duracion:string,
         numero_canciones: number,
-        imagen: string
+        imagen: string,
+        preset:number
+
 }
 
-export type CollectionAlbumsClass = {
-    id_cantante: number,
-    albumes: AlbumClass[]
-}
-
-export type song = {
-        id_cancion:number,
+export type SongClass = {
+        id:string,
         nombre_cancion:string,
         duracion:string,
         numero_pista:number
 }
 
-export type CollectionSongsClass = {
-    id_album:number,
-    canciones: song []
+export type CollectionSongsClass ={
+        id_cantante :ArtistClass['id']
+        id_album : AlbumClass['id_album']
+        songs : SongClass[]
 }
